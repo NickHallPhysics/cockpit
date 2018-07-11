ae6cbd7f713ff30fb10c84807ad967a0f3b4f814
NickHallONBI <nicholas.hall@dtc.ox.ac.uk>
2018-07-11 12:31:17 +0100

    Decided calculations shouldn't happen here

--- experiment/zStack_remote.py
+++ experiment/zStack_remote.py
@@ -3,43 +3,15 @@ from . import experiment
 
 import decimal
 import math
-import numpy as np
-import scipy.stats as stats
 
 ## Provided so the UI knows what to call this experiment.
 EXPERIMENT_NAME = 'Remote Z-stack'
 
-#Create accurate look up table for certain Z positions
-##LUT dict has key of Z positions
-LUT_array = np.loadtxt("remote_focus_LUT.txt")
-LUT = {}
-for ii in (LUT_array[:,0])[:]:
-    LUT[ii] = LUT_array[np.where(LUT_array == ii)[0][0],1:]
-
 ## This class handles classic Z-stack experiments.
 class RemoteZStackExperiment(experiment.Experiment):
+
     def __init__(self, dmHandler = None):
         self.dmHandler = dmHandler
-
-        #For Z positions which have not been calibrated, approximate with
-        #a regression of known positions.
-        ## ACTUATOR_FITS has key of actuators
-        self.no_defined_actuators = np.shape(LUT_array)[1]-1
-        self.actuator_slopes = np.zeros(self.no_defined_actuators)
-        self.actuator_intercepts = np.zeros(self.no_defined_actuators)
-
-        pos = np.sort(LUT_array[:,0])[:]
-        ac_array = np.zeros((np.shape(LUT_array)[0],self.no_defined_actuators))
-
-        count = 0
-        for ii in pos:
-            ac_array[count,:] = LUT_array[np.where(LUT_array == ii)[0][0],1:]
-            count += 1
-
-        for ii in range(self.no_defined_actuators):
-            s, i, r, p, se = stats.linregress(pos, ac_array[:,ii])
-            self.actuator_slopes[ii] = s
-            self.actuator_intercepts[ii] = i
 
     ## Create the ActionTable needed to run the experiment. We simply move to
     # each Z-slice in turn, take an image, then move to the next.

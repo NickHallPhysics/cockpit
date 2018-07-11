5fbcd208238a3274d73960201dcdbb34cfcb9a91
NickHallONBI <nicholas.hall@dtc.ox.ac.uk>
2018-07-11 12:30:43 +0100

    Vectorised calculations and put into seperate function

--- experiment/zStack_remote.py
+++ experiment/zStack_remote.py
@@ -13,31 +13,34 @@ EXPERIMENT_NAME = 'Remote Z-stack'
 ##LUT dict has key of Z positions
 LUT_array = np.loadtxt("remote_focus_LUT.txt")
 LUT = {}
-for ii in np.flip(np.sort(LUT_array[:,0])[:],0):
+for ii in (LUT_array[:,0])[:]:
     LUT[ii] = LUT_array[np.where(LUT_array == ii)[0][0],1:]
-
-#For Z positions which have not been calibrated, approximate with
-#a regression of known positions.
-## ACTUATOR_FITS has key of actuators
-ACTUATOR_FITS = {}
-no_defined_actuators = np.shape(LUT_array)[1]-1
-pos = np.sort(LUT_array[:,0])[:]
-ac_array = np.zeros((np.shape(LUT_array)[0],no_defined_actuators))
-
-#ac_pos_start = np.zeros(no_defined_actuators)
-#ac_pos = np.zeros(no_defined_actuators)
-
-count = 0
-for ii in pos:
-    ac_array[count,:] = LUT_array[np.where(LUT_array == ii)[0][0],1:]
-    count += 1
-
-for ii in range(np.shape(pos)[0]):
-    s, i, r, p, se = stats.linregress(pos, ac_array[:,ii])
-    ACTUATOR_FITS[ii] = (s, i)
 
 ## This class handles classic Z-stack experiments.
 class RemoteZStackExperiment(experiment.Experiment):
+    def __init__(self, dmHandler = None):
+        self.dmHandler = dmHandler
+
+        #For Z positions which have not been calibrated, approximate with
+        #a regression of known positions.
+        ## ACTUATOR_FITS has key of actuators
+        self.no_defined_actuators = np.shape(LUT_array)[1]-1
+        self.actuator_slopes = np.zeros(self.no_defined_actuators)
+        self.actuator_intercepts = np.zeros(self.no_defined_actuators)
+
+        pos = np.sort(LUT_array[:,0])[:]
+        ac_array = np.zeros((np.shape(LUT_array)[0],self.no_defined_actuators))
+
+        count = 0
+        for ii in pos:
+            ac_array[count,:] = LUT_array[np.where(LUT_array == ii)[0][0],1:]
+            count += 1
+
+        for ii in range(self.no_defined_actuators):
+            s, i, r, p, se = stats.linregress(pos, ac_array[:,ii])
+            self.actuator_slopes[ii] = s
+            self.actuator_intercepts[ii] = i
+
     ## Create the ActionTable needed to run the experiment. We simply move to
     # each Z-slice in turn, take an image, then move to the next.
     def generateActions(self):
@@ -47,13 +50,9 @@ class RemoteZStackExperiment(experiment.Experiment):
         numZSlices = int(math.ceil(self.zHeight / self.sliceHeight))
         #Either call or calculate actuator positions for Z position
         try:
-            ac_pos_start = LUT[self.zStart]
+            self.ac_pos_start = LUT[self.zStart]
         except KeyError:
-            ac_pos_start_dbl = []
-            for ii in range(no_defined_actuators):
-                (slope, intercept) = ACTUATOR_FITS[ii]
-                ac_pos_start_dbl.append((slope * self.zStart) + intercept)
-            ac_pos_start = np.asarray(ac_pos_start_dbl)
+            self.ac_pos_start = (self.actuator_slopes * self.zStart) + self.actuator_intercepts
         if self.zHeight > 1e-6:
             # Non-2D experiment; tack on an extra image to hit the top of
             # the volume.
@@ -65,20 +64,16 @@ class RemoteZStackExperiment(experiment.Experiment):
 
             #Either call or calculate actuator positions for Z position
             try:
-                ac_pos_dbl = LUT[zTarget]
+                ac_pos = LUT[zTarget]
             except KeyError:
-                ac_pos_dbl = []
-                for ii in range(no_defined_actuators):
-                    (slope, intercept) = ACTUATOR_FITS[ii]
-                    ac_pos_dbl.append((slope * self.zStart) + intercept)
-                ac_pos = np.asarray(ac_pos_dbl)
-
+                ac_pos = (self.actuator_slopes * zTarget) + self.actuator_intercepts
             motionTime, stabilizationTime = 0, 0
             if prevAltitude is not None:
                 motionTime, stabilizationTime = self.zPositioner.getMovementTime(prevAltitude, zTarget)
             curTime += motionTime
             #table.addAction(curTime, self.zPositioner, zTarget)
-            table.addAction(curTime, self.dmHandler, ac_pos)
+            if self.dmHandler is not None:
+                table.addAction(curTime, self.dmHandler, ac_pos)
             curTime += stabilizationTime
             prevAltitude = zTarget
 
@@ -90,14 +85,16 @@ class RemoteZStackExperiment(experiment.Experiment):
                 curTime += decimal.Decimal('1e-10')
             # Hold the Z motion flat during the exposure.
             #table.addAction(curTime, self.zPositioner, zTarget)
-            table.addAction(curTime, self.dmHandler, ac_pos)
+            if self.dmHandler is not None:
+                table.addAction(curTime, self.dmHandler, ac_pos)
 
         # Move back to the start so we're ready for the next rep.
         motionTime, stabilizationTime = self.zPositioner.getMovementTime(
                 self.zHeight, 0)
         curTime += motionTime
         #table.addAction(curTime, self.zPositioner, self.zStart)
-        table.addAction(curTime, self.dmHandler, ac_pos_start)
+        if self.dmHandler is not None:
+            table.addAction(curTime, self.dmHandler, self.ac_pos_start)
         # Hold flat for the stabilization time, and any time needed for
         # the cameras to be ready. Only needed if we're doing multiple
         # reps, so we can proceed immediately to the next one.
@@ -109,8 +106,9 @@ class RemoteZStackExperiment(experiment.Experiment):
                             self.getTimeWhenCameraCanExpose(table, camera))
         #table.addAction(max(curTime + stabilizationTime, cameraReadyTime),
         #        self.zPositioner, self.zStart)
-        table.addAction(max(curTime + stabilizationTime, cameraReadyTime),
-                        self.dmHandler, ac_pos_start)
+        if self.dmHandler is not None:
+            table.addAction(max(curTime + stabilizationTime, cameraReadyTime),
+                        self.dmHandler, self.ac_pos_start)
 
         return table
 
